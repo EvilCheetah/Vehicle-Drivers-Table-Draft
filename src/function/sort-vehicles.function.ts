ac91860292fc10b8f63753cda51dd943b1f28a0a
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-07-06 15:11:23 -0700

    add: switch case for sorting function

---
+++ src/function/sort-vehicles.function.ts
@@ -0,0 +1,36 @@
+export function sort_vehicle(accessor, state)
+{
+    switch ( accessor )
+    {
+        case('unit'):
+            
+            
+        case('driver_name'):
+            
+            
+        case('driver_license'):
+            
+            
+        case('authorization'):
+            
+            
+        case('registration'):
+            
+            
+        case('annual_inspection'):
+            
+            
+        case('insurance_general'):
+            
+            
+        case('insurance_cargo'):
+            
+            
+        case('insurance_liability'):
+            
+            
+        case('insurance_auto'):
+            
+            
+    }
+}
